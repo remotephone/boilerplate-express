0cc760d4e20dff5770761675947ef509df631953
remotephone <remotephone@gmail.com>
2021-11-23 19:54:16 -0600

    fix

--- myApp.js
+++ myApp.js
@@ -15,9 +15,9 @@ app.get("/", (req, res) => {
 )
 
 app.get("/json", (req, res) => {
-    if (process.env.MESSAGE_STYLE == "uppercase"){
+    if (process.env.MESSAGE_STYLE === "uppercase"){
         res.json({
-            message: "HELLO JSON"
+            message: "Hello json".toUpperCase()
           });
       
     } else {
