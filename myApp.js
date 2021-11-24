7723c9d5389b42d5f432456c0c4c5177094bdd9c
remotephone <remotephone@gmail.com>
2021-11-23 18:00:54 -0600

    3.1

--- myApp.js
+++ myApp.js
@@ -6,12 +6,11 @@ console.log("Hello World")
 
 
 app.get("/", (req, res) => {
-    absolutePath = __dirname + views/index.html;
+    absolutePath = __dirname + "/views/index.html";
     res.sendFile(absolutePath);
   }
 )
 
-absolutePath = __dirname + relativePath/file.ext
 
 app.get("/test", (req, res) => {
     res.send('Hello Express');
